dff9d903afaf10d9faaf96842474dc6b512b8f04
Bastian Wilhelm <bw-dev@mailbox.org>
2019-08-31 11:58:58 +0200

    restructure tests

--- src/test/groovy/de/baswil/spring/proxy/ProxySettingsFactorySpec.groovy
+++ src/test/groovy/de/baswil/spring/proxy/ProxySettingsFactorySpec.groovy
@@ -10,8 +10,132 @@ class ProxySettingsFactorySpec extends Specification {
         proxySettingsFactory = new ProxySettingsFactory()
     }
 
+    def "create proxy without variables"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(
+                null,
+                null,
+                null,
+                null,
+                null)
+
+        then:
+        currentResult == null
+    }
+
     @Unroll
-    def "create proxy with parameters: #osProperty, #javaHost, #javaPort, #javaUser, #javaPassword"() {
+    def "create proxy only from OS environment variables (env. variable: #osProperty)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(
+                osProperty,
+                null,
+                null,
+                null,
+                null
+        )
+
+        then:
+        currentResult == expectedResult
+
+        where:
+        osProperty                                  || expectedResult
+        "http://localhost"                          || new ProxySettings(host: "localhost", port: null, user: null, password: null)
+        "http://localhost:8080"                     || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
+        "http://user@localhost:8080"                || new ProxySettings(host: "localhost", port: 8080, user: "user", password: null)
+        "http://user:password@localhost:8080"       || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password")
+        "http://user:password:colon@localhost:8080" || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password:colon")
+    }
+
+    @Unroll
+    def "create proxy only from OS environment variables with wrong urls (env. variable: #osProperty)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(
+                osProperty,
+                null,
+                null,
+                null,
+                null
+        )
+
+        then:
+        currentResult == null
+
+        where:
+        osProperty << ["localhost", "http:/localhost"]
+    }
+
+    @Unroll
+    def "create proxy only from java Properties (host: #javaHost, port: #javaPort, user: #javaUser, password: #javaPassword)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(null, javaHost, javaPort, javaUser, javaPassword)
+
+        then:
+        currentResult == expectedResult
+
+        where:
+        javaHost    || javaPort || javaUser || javaPassword || expectedResult
+        "localhost" || null     || null     || null         || new ProxySettings(host: "localhost", port: null, user: null, password: null)
+        "localhost" || "8080"   || null     || null         || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
+        "localhost" || "8080"   || "user"   || "password"   || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password")
+    }
+
+    @Unroll
+    def "create proxy only from java Properties without hostname (prot: #javaPort, user: #javaUser, password: #javaPassword)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(
+                null,
+                null,
+                javaPort,
+                javaUser,
+                javaPassword
+        )
+
+        then:
+        currentResult == null
+
+        where:
+        javaPort || javaUser || javaPassword
+        "8080"   || null     || null
+        null     || "user"   || null
+        null     || null     || "password"
+        "8080"   || "user"   || null
+        "8080"   || null     || "password"
+        null     || "user"   || "password"
+        "8080"   || "user"   || "password"
+    }
+
+    def "create proxy only from java Properties with password and without username"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(null, "localhost", "8080", null, "password")
+
+        then:
+        with(currentResult, {
+            host == "localhost"
+            port == 8080
+            user == null
+            password == null
+        })
+    }
+
+    @Unroll
+    def "create proxy only from java Properties with wrong port number (port: #javaPort)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(null, "localhost", javaPort, "user", "password")
+
+        then:
+        with(currentResult, {
+            host == "localhost"
+            port == null
+            user == "user"
+            password == "password"
+        });
+
+        where:
+        javaPort << ["-1", "bla"]
+    }
+
+    @Unroll
+    def "override proxy properties from OS (env. variable: #osProperty, host: #javaHost, port: #javaPort, user: #javaUser, password: #javaPassword)"() {
         when:
         def currentResult = proxySettingsFactory.createProxySettings(osProperty, javaHost, javaPort, javaUser, javaPassword)
 
@@ -19,43 +143,26 @@ class ProxySettingsFactorySpec extends Specification {
         currentResult == expectedResult
 
         where:
-        osProperty         || javaHost || javaPort || javaUser || javaPassword || expectedResult
-        null               || null     || null     || null     || null         || null
-        // Only Environment Variable
-        "http://localhost" || null     || null     || null     || null         || new ProxySettings(host: "localhost", port: null, user: null, password: null)
-        "http://localhost:8080" || null || null || null || null || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
-        "http://user@localhost:8080" || null || null || null || null || new ProxySettings(host: "localhost", port: 8080, user: "user", password: null)
-        "http://user:password@localhost:8080" || null || null || null || null || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password")
-        "http://user:password:colon@localhost:8080" || null || null || null || null || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password:colon")
-        // Only Environment Variable, but with wrong url format
-        "localhost" || null     || null     || null     || null         || null
-        "http:/localhost" || null     || null     || null     || null         || null
-        // Only Java Properties
-        null || "localhost" || null || null || null || new ProxySettings(host: "localhost", port: null, user: null, password: null)
-        null || "localhost" || "8080" || null || null || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
-        null || "localhost" || "8080" || "user" || "password" || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "password")
-        // Only Java Properties, with password but without user
-        null || "localhost" || "8080" || null || "password" || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
-        // Only Java Properties, but without host:
-        null || null || "8080" || null || null || null
-        null || null || null || "user" || null || null
-        null || null || null || null || "password" || null
-        null || null || "8080" || "user" || null || null
-        null || null || "8080" || null || "password" || null
-        null || null || null || "user" || "password" || null
-        null || null || "8080" || "user" || "password" || null
-        // Only Java Properties, with wrong port number:
-        null || "localhost" || "-1" || "user" || "password" || new ProxySettings(host: "localhost", port: null, user: "user", password: "password")
-        null || "localhost" || "bla" || "user" || "password" || new ProxySettings(host: "localhost", port: null, user: "user", password: "password")
-        // Override Environment Properties
-        "http://user:password@localhost:8080" || "tsohlacol" || null || null || null || new ProxySettings(host: "tsohlacol", port: 8080, user: "user", password: "password")
-        "http://user:password@localhost:8080" || null || "9090" || null || null || new ProxySettings(host: "localhost", port: 9090, user: "user", password: "password")
-        "http://user:password@localhost:8080" || null || null || "resu" || null || new ProxySettings(host: "localhost", port: 8080, user: "resu", password: "password")
-        "http://user:password@localhost:8080" || null || null || null || "drowssap" || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "drowssap")
-        // Reset Environment Properties
-        "http://user:password@localhost:8080" || null || null || null || "" || new ProxySettings(host: "localhost", port: 8080, user: "user", password: null)
-        "http://user:password@localhost:8080" || null || null || "" || null || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
-        "http://user:password@localhost:8080" || null || "" || null || null || new ProxySettings(host: "localhost", port: null, user: "user", password: "password")
-        "http://user:password@localhost:8080" || "" || null || null || null || null
+        osProperty                            || javaHost    || javaPort || javaUser || javaPassword || expectedResult
+        "http://user:password@localhost:8080" || "tsohlacol" || null     || null     || null         || new ProxySettings(host: "tsohlacol", port: 8080, user: "user", password: "password")
+        "http://user:password@localhost:8080" || null        || "9090"   || null     || null         || new ProxySettings(host: "localhost", port: 9090, user: "user", password: "password")
+        "http://user:password@localhost:8080" || null        || null     || "resu"   || null         || new ProxySettings(host: "localhost", port: 8080, user: "resu", password: "password")
+        "http://user:password@localhost:8080" || null        || null     || null     || "drowssap"   || new ProxySettings(host: "localhost", port: 8080, user: "user", password: "drowssap")
+    }
+
+    @Unroll
+    def "unset proxy properties from OS with empty string (env. variable: #osProperty, host: #javaHost, port: #javaPort, user: #javaUser, password: #javaPassword)"() {
+        when:
+        def currentResult = proxySettingsFactory.createProxySettings(osProperty, javaHost, javaPort, javaUser, javaPassword)
+
+        then:
+        currentResult == expectedResult
+
+        where:
+        osProperty                            || javaHost    || javaPort || javaUser || javaPassword || expectedResult
+        "http://user:password@localhost:8080" || null        || null     || null     || ""           || new ProxySettings(host: "localhost", port: 8080, user: "user", password: null)
+        "http://user:password@localhost:8080" || null        || null     || ""       || null         || new ProxySettings(host: "localhost", port: 8080, user: null, password: null)
+        "http://user:password@localhost:8080" || null        || ""       || null     || null         || new ProxySettings(host: "localhost", port: null, user: "user", password: "password")
+        "http://user:password@localhost:8080" || ""          || null     || null     || null         || null
     }
 }
